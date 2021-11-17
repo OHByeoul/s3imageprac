6f40fbf13c33f347c9d585c723befeeabba0cadc
오한별 <suriessay@naver.com>
2021-11-17 23:58:51 +0900

    메뉴조회 작성

--- src/main/java/com/s3/prac/menu/MenuController.java
+++ src/main/java/com/s3/prac/menu/MenuController.java
@@ -4,6 +4,7 @@ import com.s3.prac.dto.MenuRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Required;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -15,6 +16,11 @@ public class MenuController {
     @PostMapping("/menus")
     public void create(@RequestBody MenuRequest request) {
         menuService.create(request);
+    }
+
+    @GetMapping("/menu")
+    public String find() {
+        return menuService.find();
     }
 
 }
--- src/main/java/com/s3/prac/menu/MenuService.java
+++ src/main/java/com/s3/prac/menu/MenuService.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -19,5 +20,13 @@ public class MenuService {
 
         Menu createdMenu = Menu.createMenu(menuName, price);
         menuRepository.save(createdMenu);
+    }
+
+    public String find() {
+        int random = (int)Math.random()*100;
+        List<Menu> allMenu = menuRepository.findAll();
+        int size = allMenu.size();
+        int index = random%size;
+        return allMenu.get(index).getName();
     }
 }
